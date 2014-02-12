77b93a2ace5960adbbf60a524b3245859b5aefd4
mcegan26 <mcegan26@hotmail.co.uk>
2014-02-12 19:11:25 +0000

    More unused usings removed

--- SecureHeartbeat/Models/StatusModel.cs
+++ SecureHeartbeat/Models/StatusModel.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ComponentModel;
-using System.Diagnostics;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
 
 namespace SecureHeartbeat.Models
 {
